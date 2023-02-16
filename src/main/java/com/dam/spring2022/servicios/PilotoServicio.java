76e32d7b8e31ace8e6a3d170059e1f1887c95f0f
carpui <infocarlosexpuig@gmail.com>
2023-02-16 16:39:05 +0100

    Creado

---
+++ src/main/java/com/dam/spring2022/servicios/PilotoServicio.java
@@ -0,0 +1,9 @@
+package com.dam.proyectospring.servicios;
+
+import com.dam.proyectospring.modelos.Piloto;
+
+import java.util.List;
+
+public interface PilotoServicio {
+    List<Piloto> findAllPilotos();
+}
